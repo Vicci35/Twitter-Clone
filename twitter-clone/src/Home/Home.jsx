39b9251d9190a94613f6f5c900cb4a214adb0b41
VT <vithanh10@hotmail.com>
2025-05-06 17:21:12 +0200

    Skapade jsx-fil för home

---
+++ twitter-clone/src/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Homepage = () => {
+    return (
+        <>
+        <div>
+        <h2>Home</h2>
+        <textarea
+            type="text"
+            placeholder="Yooo mama"
+        ></textarea>
+        </div>
+        </>
+    )
+}
