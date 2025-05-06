a94c0df4770628949dadc1bf1b6d835ad72b87d8
Victoria Tobisson Lind <toja.1999@hotmail.com>
2025-05-06 16:33:28 +0200

    Login form

---
+++ twitter-clone/src/Login/login.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const LogIn = () => {
+  return (
+    <>
+      <div>
+        <h1>Logga in på Twitter</h1>
+        <input
+          type="text"
+          placeholder="Mobil, e-postadress eller användarnamn"
+        ></input>
+      </div>
+    </>
+  );
+};
+
+export default LogIn;
