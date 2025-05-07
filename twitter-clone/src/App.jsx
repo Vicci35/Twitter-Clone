996de9969d4a23c65a32900907a6f263187fd37f
VT <vithanh10@hotmail.com>
2025-05-07 15:39:48 +0200

    Ändrade lite i app jsx-filen för att kunna se det jag gör på localhost

--- twitter-clone/src/App.jsx
+++ twitter-clone/src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LogIn from "./Login/login";
+/*import LogIn from "./Login/login";*/
+import Home from "../src/Home/Home.jsx"
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LogIn />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
