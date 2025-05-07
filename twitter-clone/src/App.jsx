ff157a3ea3d0f10a06be06205f3a7c43baae256f
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-05-07 19:55:47 +0200

    Fix: Import in App component

--- twitter-clone/src/App.jsx
+++ twitter-clone/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LogIn from "./Login/login";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import LogIn from "./components/Login/Login";
+import SignUp from "./components/SignUp/SignUp";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LogIn />} />
+        <Route path="sign-up" element={<SignUp />} />
       </Routes>
     </Router>
   );
