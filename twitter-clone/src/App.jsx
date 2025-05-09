59b0d26cd6883d580326bc9588561b049d8feeae
Victoria Tobisson Lind <toja.1999@hotmail.com>
2025-05-09 13:13:35 +0200

    changed name on the component LogIn to Loginstep1

--- twitter-clone/src/App.jsx
+++ twitter-clone/src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import LogIn from "./components/Login/LoginStep1";
 import LogInStep2 from "./components/Login/LogInStep2";
 import SignUp from "./components/SignUp/SignUp";
+import LoginStep1 from "./components/Login/LoginStep1";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LogIn />} />
+        <Route path="/" element={<LoginStep1 />} />
         <Route path="/login/password" element={<LogInStep2 />} />
         <Route path="sign-up" element={<SignUp />} />
       </Routes>
