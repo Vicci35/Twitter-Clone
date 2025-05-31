503c4da5497b75f79d351449e1c89620e73fa703
Viccan <toja.1999@hotmail.com>
2025-05-31 22:44:14 +0200

    minor fix with the E2E test for log in

--- twitter-clone/e2e-tester/login.spec.js
+++ twitter-clone/e2e-tester/login.spec.js
@@ -1,21 +1,17 @@
-
 import { test, expect } from '@playwright/test';
 
 test('login flow E2E', async ({ page }) => {
   
-  await page.goto('http://localhost:3000/login'); 
-
-
+  await page.goto('http://localhost:5173');
+  
   await page.fill('input[placeholder="E-postadress eller användarnamn"]', 'nina@live.se');
-  await page.click('button:has-text("Nästa")');
+  await page.click('button.next');
 
   await expect(page).toHaveURL(/\/login\/password/);
-
 
   await page.fill('input[placeholder="Lösenord"]', 'Nina123!');
   await page.click('button.LoggaIn');
 
- 
-  await expect(page).toHaveURL('http://localhost:3000/dashboard');
-  await expect(page.locator('text=Välkommen')).toBeVisible(); 
+  await expect(page).toHaveURL('/dashboard');
+  await expect(page.locator('text=Nina123')).toBeVisible(); 
 });
